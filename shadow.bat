763e14c70916e9a6ca3ea70175bfda2e64e5260c
f1uzz <fluzzz142857@gmail.com>
2020-02-22 18:08:34 -0800

    Remove outdated comment

--- shadow.bat
+++ shadow.bat
@@ -3,7 +3,6 @@
 rem You can make a shortcut of this file and then place it on your Desktop to run it easily
 rem by right clicking on this file, choosing "Create shortcut", and then dragging
 rem the shortcut to your desktop
-rem Replace the line below with whatever you want to run
 python -m shadow
 
 
