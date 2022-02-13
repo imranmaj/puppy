4fe81448bf160a8b5a134442d53094a104728a28
imranmaj <49664304+imranmaj@users.noreply.github.com>
2022-02-13 02:13:29 -0800

    Add build CI

--- puppy/__main__.py
+++ puppy/__main__.py
@@ -1,3 +1,21 @@
+import os
+import platform
+import sys
+import subprocess
+
 from puppy import main
 
-main()
+
+if platform.system() == "Darwin" and not sys.stdout.isatty():
+    # packaged mac app doesn't launch with a terminal
+    subprocess.run(
+        [
+            "open",
+            "-a",
+            "Terminal",
+            os.path.dirname(os.path.realpath(sys.argv[0]))
+            + "/puppy.app/Contents/MacOS/puppy",
+        ]
+    )
+else:
+    main()
