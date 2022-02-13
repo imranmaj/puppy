d4aae950aa848c249898502b826d5689e5c8d2a7
imranmaj <49664304+imranmaj@users.noreply.github.com>
2022-02-13 05:32:36 -0800

    Fix missing terminal on mac

--- puppy/__main__.py
+++ puppy/__main__.py
@@ -1,21 +1,29 @@
 import os
+from pathlib import Path
 import platform
 import sys
 import subprocess
 
-from puppy import main
-
 
 if platform.system() == "Darwin" and not sys.stdout.isatty():
     # packaged mac app doesn't launch with a terminal
+    # https://github.com/pyinstaller/pyinstaller/issues/5154
     subprocess.run(
         [
             "open",
             "-a",
             "Terminal",
-            os.path.dirname(os.path.realpath(sys.argv[0]))
-            + "/puppy.app/Contents/MacOS/puppy",
+            Path(os.path.realpath(sys.argv[0])).parent / "__main__",
         ]
     )
 else:
+    loc = Path(os.path.realpath(sys.argv[0])).parent
+    os.chdir(loc)
+    # get outside .app on mac
+    for i, part in enumerate(loc.parts):
+        if part.endswith(".app"):
+            os.chdir(Path(*loc.parts[:i]))
+
+    from puppy import main
+
     main()
